170d9f1c2a0c0a9fd98d3b53214d2a1198740af7
Steven ott <stevenott@charter.net>
2025-01-05 09:21:11 -0600

    Adjust spacing on home page

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="bg-zinc-900/50">
       <Intro />
-      <div className="flex flex-col xl:gap-10 xl:flex-row">
+      <div className="flex flex-col pb-10 xl:gap-10 xl:flex-row">
         <div className="xl:w-2/3">
           <Experience />
         </div>
